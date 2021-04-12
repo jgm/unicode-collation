1721be4f25e7fd7794ac54daf1c913ebe0c50963
John MacFarlane <jgm@berkeley.edu>
2021-04-12 09:35:04 -0700

    Have doctest set locale to UTF-8.

--- test/doctests.hs
+++ test/doctests.hs
@@ -1,4 +1,7 @@
 import Test.DocTest
+import System.Environment (setEnv)
 
 main :: IO ()
-main = doctest ["-isrc", "src/UnicodeCollation.hs"]
+main = do
+  setEnv "LC_ALL" "C.UTF-8"
+  doctest ["-isrc", "src/UnicodeCollation.hs"]
