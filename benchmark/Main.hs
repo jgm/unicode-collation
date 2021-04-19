23afac6a7a2dfe504dbe05d5b7d0f51a08c7c951
John MacFarlane <jgm@berkeley.edu>
2021-04-18 17:16:17 -0700

    Add a benchmark for texts of length 1.
    This helps demonstrate the effect of normalization.
    text-icu is more efficient because it normalizes Texts
    incrementally and can avoid normalizing a whole string
    once enough has been read to make a determination of the
    ordering.  We can see this by noting that the gap between
    our library and text-icu is reduced substantially when
    normalization is turned off or when the strings are short
    (so that incremental normalization doesn't help).

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -5,6 +5,7 @@ module Main (main) where
 import Test.Tasty.Bench
 import Test.QuickCheck
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.ICU as ICU
 import Data.Text.ICU.Collate (Attribute(..), Strength(..))
 import Text.Collate
@@ -15,7 +16,10 @@ import Data.List (sortBy)
 main :: IO ()
 main = do
   (randomTexts :: [Text]) <- generate (infiniteListOf arbitrary)
+  (randomSingletonTexts :: [Text]) <-
+    generate (infiniteListOf (arbitrary `suchThat` (\t -> T.length t == 1)))
   let tenThousand = take 10000 randomTexts
+  let tenThousandSingletons = take 10000 randomSingletonTexts
   let icuCollator lang = ICU.collatorWith (ICU.Locale lang)
                           [NormalizationMode True, Strength Quaternary]
   defaultMain
@@ -29,5 +33,9 @@ main = do
         (whnf (sortBy (ICU.collate (icuCollator "zh"))) tenThousand)
     , bench "sort a list of 10000 random Texts (en-u-kk-false = no normalize)"
         (whnf (sortBy (collate (collatorFor "en-u-kk-false"))) tenThousand)
+    , bench "sort a list of 10000 random Texts of length 1 (en)"
+        (whnf (sortBy (collate (collatorFor "en"))) tenThousandSingletons)
+    , bench "sort same list with text-icu (en)"
+        (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousandSingletons)
     ]
 
