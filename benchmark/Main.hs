b9eff134f8c76de26b7365416fa2c30d87e5714c
John MacFarlane <jgm@berkeley.edu>
2021-04-25 15:21:31 -0700

    Improve benchmark suite.
    More realistic examples, with a smaller character set,
    leading to more nontrivial sorts, and also many composed
    characters.

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -9,20 +9,23 @@ import qualified Data.Text as T
 import qualified Data.Text.ICU as ICU
 import Data.Text.ICU.Collate (Attribute(..), Strength(..))
 import Text.Collate
+import Text.Collate.Normalize (toNFD)
 import Test.QuickCheck.Instances.Text ()
 import Data.List (sortBy)
-import Data.Char (isAscii)
+import Data.Char (isAscii, ord, chr)
 
 main :: IO ()
 main = do
   (randomTexts :: [Text]) <- generate (infiniteListOf arbitrary)
-  (randomSingletonTexts :: [Text]) <-
-    generate (infiniteListOf (arbitrary `suchThat` (\t -> T.length t == 1)))
+  (randomLatinStrings :: [String]) <-
+      generate (infiniteListOf (listOf (elements latinChars)))
   (randomAsciiTexts :: [Text]) <-
     generate (infiniteListOf (arbitrary `suchThat` T.all isAscii))
   let tenThousand = take 10000 randomTexts
+  let tenThousandLatin = map T.pack $ take 10000 randomLatinStrings
+  let tenThousandLatinNFD = map (T.pack . map chr . toNFD . map ord . T.unpack)
+                              tenThousandLatin
   let tenThousandString = map T.unpack tenThousand
-  let tenThousandSingletons = take 10000 randomSingletonTexts
   let tenThousandAscii = take 10000 randomAsciiTexts
   let tenThousandLong = map ("A bcd efgh ijklmnop qrs tuv WxyZ" <>) tenThousand
   let icuCollator lang = ICU.collatorWith (ICU.Locale lang)
@@ -33,24 +36,23 @@ main = do
         (whnf (sortBy (collate (collatorFor "en"))) tenThousand)
     , bench "sort same list with text-icu (en)"
         (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousand)
-    , bench "sort a list of 10000 random Texts (zh)"
-        (whnf (sortBy (collate (collatorFor "zh"))) tenThousand)
-    , bench "sort same list with text-icu (zh)"
-        (whnf (sortBy (ICU.collate (icuCollator "zh"))) tenThousand)
-    , bench "sort a list of 10000 random Strings (en)"
-        (whnf (sortBy collateString) tenThousandString)
+    , bench "sort a list of 10000 Texts (composed latin) (en)"
+        (whnf (sortBy (collate (collatorFor "en"))) tenThousandLatin)
+    , bench "sort same list with text-icu (en)"
+        (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousandLatin)
+    , bench "sort a list of 10000 Texts (normalized latin) (en-u-kk-false)"
+        (whnf (sortBy (collate (collatorFor "en-u-kk-false"))) tenThousandLatinNFD)
     , bench "sort a list of 10000 ASCII Texts (en)"
         (whnf (sortBy (collate (collatorFor "en"))) tenThousandAscii)
     , bench "sort same list with text-icu (en)"
         (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousandAscii)
-    , bench "sort a list of 10000 random Texts (en-u-kk-false = no normalize)"
-        (whnf (sortBy (collate (collatorFor "en-u-kk-false"))) tenThousand)
-    , bench "sort a list of 10000 random Texts of length 1 (en)"
-        (whnf (sortBy (collate (collatorFor "en"))) tenThousandSingletons)
-    , bench "sort same list with text-icu (en)"
-        (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousandSingletons)
-    , bench "sort a list of 10000 random Texts that agree in first 32 chars (en)"
+    , bench "sort a list of 10000 random Texts that agree in first 32 chars"
         (whnf (sortBy (collate (collatorFor "en"))) tenThousandLong)
     , bench "sort same list with text-icu (en)"
         (whnf (sortBy (ICU.collate (icuCollator "en"))) tenThousandLong)
+    , bench "sort a list of 10000 random Strings (en)"
+        (whnf (sortBy collateString) tenThousandString)
     ]
+
+latinChars :: [Char]
+latinChars = "ḀḁḂḃḄḅḆḇḈḉḊḋḌḍḎḏḐḑḒḓḔḕḖḗḘḙḚḛḜḝḞḟḠḡḢḣḤḥḦḧḨḩḪḫḬḭḮḯḰḱḲḳḴḵḶḷḸḹḺḻḼḽḾḿṀṁṂṃṄṅṆṇṈṉṊṋṌṍṎṏṐṑṒṓṔṕṖṗṘṙṚṛṜṝṞṟṠṡṢṣṤṥṦṧṨṩṪṫṬṭṮṯṰṱṲṳṴṵṶṷṸṹṺṻṼṽṾṿ‐‑‒–—―‖‗‘’‚‛“”„‟†‡•‣․‥…"
